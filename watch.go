dd68de280bdbd68d06e750398694f90f239c0df5
Ian Mikhailov <ian.mikhailov@gmail.com>
2019-05-16 14:39:25 +0300

    fix

--- watch.go
+++ watch.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 
 	"github.com/hashicorp/consul/api"
-	"github.com/hashicorp/consul/watch"
+	"github.com/hashicorp/consul/api/watch"
 	"go.uber.org/zap"
 )
 
@@ -18,21 +18,22 @@ type (
 	}
 
 	ConsulWatcher struct {
-		logger *zap.Logger
-		plan   *watch.Plan
+		logger     *zap.Logger
+		plan       *watch.Plan
 		consulAddr Address
 
 		once sync.Once
 
-		ch chan []Address
+		ch  chan []Address
 		err error
 	}
-	LogWriter struct {
+
+	logger struct {
 		logger *zap.Logger
 	}
 )
 
-func (w *LogWriter) Write(data []byte) (int, error) {
+func (w *logger) Write(data []byte) (int, error) {
 	w.logger.Warn(string(data))
 	return len(data), nil
 }
@@ -49,14 +50,18 @@ func NewConsulWatcher(log *zap.Logger, consulAddr, name, dc, tag string) (*Consu
 		return nil, err
 	}
 
-
-	w := &ConsulWatcher{logger: log, consulAddr: consulAddr, plan: plan, ch:  make(chan []Address)}
-	w.plan.LogOutput = &LogWriter{log.With(zap.String("module", "consul_watcher"))}
+	w := &ConsulWatcher{
+		logger:     log,
+		consulAddr: consulAddr,
+		plan:       plan,
+		ch:         make(chan []Address),
+	}
+	w.plan.LogOutput = &logger{log.With(zap.String("module", "consul_watcher"))}
 	w.plan.Handler = func(i uint64, data interface{}) {
 		var addrs []Address
 		for _, srv := range data.([]*api.ServiceEntry) {
 			host := srv.Service.Address
-			if host != "" {
+			if host == "" {
 				host = srv.Node.Address
 			}
 
@@ -68,14 +73,14 @@ func NewConsulWatcher(log *zap.Logger, consulAddr, name, dc, tag string) (*Consu
 	return w, nil
 }
 
-func(w *ConsulWatcher) Watch() (<-chan []Address, error) {
+func (w *ConsulWatcher) Watch() (<-chan []Address, error) {
 	w.once.Do(func() {
 		go func() {
+			defer func() { close(w.ch) }()
 			if err := w.plan.Run(w.consulAddr); err != nil {
 				w.err = err
 				w.logger.Error("running watch plan error", zap.String("consul_address", w.consulAddr), zap.Error(w.err))
 			}
-			close(w.ch)
 		}()
 	})
 
